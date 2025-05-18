f52d1136ea8f4d7d8a949882b27d384b3cb49790
RenatoMoura25 <rearuom@gmail.com>
2025-05-18 22:27:44 +0000

    Criei o Printf

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,93 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAMANHO 5
+#define NAVIOS 3
+
+void inicializar_tabuleiro(char tabuleiro[TAMANHO][TAMANHO]) {
+    for (int i = 0; i < TAMANHO; i++) {
+        for (int j = 0; j < TAMANHO; j++) {
+            tabuleiro[i][j] = '~';
+        }
+    }
+}
+
+void exibir_tabuleiro(char tabuleiro[TAMANHO][TAMANHO]) {
+    printf("  ");
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d ", i);
+        for (int j = 0; j < TAMANHO; j++) {
+            printf("%c ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void posicionar_navios(char tabuleiro[TAMANHO][TAMANHO]) {
+    int contador = 0;
+    while (contador < NAVIOS) {
+        int linha = rand() % TAMANHO;
+        int coluna = rand() % TAMANHO;
+
+        if (tabuleiro[linha][coluna] == '~') {
+            tabuleiro[linha][coluna] = 'N'; // N = Navio
+            contador++;
+        }
+    }
+}
+
+void jogar() {
+    char tabuleiro[TAMANHO][TAMANHO];
+    char tabuleiro_oculto[TAMANHO][TAMANHO];
+    int navios_restantes = NAVIOS;
+    int tentativas = 0;
+
+    inicializar_tabuleiro(tabuleiro);
+    inicializar_tabuleiro(tabuleiro_oculto);
+
+    posicionar_navios(tabuleiro);
+
+    printf("Bem-vindo à Batalha Naval!\n");
+
+    while (navios_restantes > 0) {
+        int linha, coluna;
+        exibir_tabuleiro(tabuleiro_oculto);
+
+        printf("Escolha uma linha (0-%d): ", TAMANHO - 1);
+        scanf("%d", &linha);
+        printf("Escolha uma coluna (0-%d): ", TAMANHO - 1);
+        scanf("%d", &coluna);
+
+        if (linha < 0 || linha >= TAMANHO || coluna < 0 || coluna >= TAMANHO) {
+            printf("Coordenadas inválidas! Tente novamente.\n");
+            continue;
+        }
+
+        tentativas++;
+
+        if (tabuleiro[linha][coluna] == 'N') {
+            printf("Acertou um navio!\n");
+            tabuleiro_oculto[linha][coluna] = 'X'; // X = Acertou
+            tabuleiro[linha][coluna] = '~';
+            navios_restantes--;
+        } else if (tabuleiro_oculto[linha][coluna] == 'O' || tabuleiro_oculto[linha][coluna] == 'X') {
+            printf("Você já atirou aqui!\n");
+        } else {
+            printf("Água!\n");
+            tabuleiro_oculto[linha][coluna] = 'O'; // O = Água
+        }
+    }
+
+    printf("Parabéns! Você venceu em %d tentativas.\n", tentativas);
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+    srand(time(NULL));
+    jogar();
     return 0;
 }
